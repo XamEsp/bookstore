4a241a07c2c2ebd165b7b781d7fa0e29d1846d32
Rodrigo Juarez <rodrigomjuarez@gmail.com>
2020-05-24 11:45:15 -0300

    Xam.forms project added

---
+++ Bookstore.Mobile/Bookstore.Mobile/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+using Bookstore.Api.Client.Models;
+using Bookstore.Api.Client.Services;
+
+namespace Bookstore.Mobile.ViewModels
+{
+    public class MainPageViewModel
+    {
+        // TODO: move all books related functionality to BooksPage/BooksPageViewModel
+        public ObservableCollection<Book> Books { get; set; }
+
+        private readonly BookService _bookService;
+        public MainPageViewModel()
+        {
+            _bookService = new BookService();
+            Books = new ObservableCollection<Book>();
+        }
+
+        public void Initialize()
+        {
+            var books = _bookService.Get();
+
+            Books.Clear();
+
+            foreach (var book in Books)
+            {
+                Books.Add(book);
+            }
+        }
+
+
+
+
+    }
+}
